af2405ae99638d1ca81fd0641d70d842a3d75f91
ASP1234 <nimish_singhal26@yahoo.com>
2015-08-22 21:03:32 +0530

    Module 1: Remove whitespaces and comments from Assembly language

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+void scan1(FILE *fptr,FILE *inptr)
+{
+    int pc=0;
+    char c;
+    bool newlineFlag=true;
+
+    while(fscanf(fptr,"%c",&c)!=EOF)
+        if(c=='\n'&&!newlineFlag)
+            {
+                newlineFlag=true;
+                fprintf(inptr,"\n");
+                pc++;
+            }
+
+        else
+            if(c!='\n')
+        switch(c)
+        {
+            case '/':   fscanf(fptr,"%c",&c);
+                        if(c=='/')
+                            while(c!='\n')
+                                fscanf(fptr,"%c",&c);
+                            if(!newlineFlag)
+                            fprintf(inptr,"\n");
+                            break;
+
+            case ' ': break;
+            default: fprintf(inptr,"%c",c);
+                     newlineFlag=false;
+                     break;
+        }
+
+        printf("PC: %d",pc);
+}
+
+
+int main()
+{
+    FILE *fptr,*inptr;
+    char fname[20];
+
+    printf("Enter the filename: ",fname);
+    scanf("%s",fname);
+
+    fptr=fopen(fname,"r");
+    inptr=fopen("main.asm","w");
+
+    if(!fptr&&!inptr)
+    return 1;
+
+    else
+    scan1(fptr,inptr);
+    return 0;
+}
