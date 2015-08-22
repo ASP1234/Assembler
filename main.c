b2b7b0cd8b6a1e87943c2e6564d687d070e19aa4
ASP1234 <nimish_singhal26@yahoo.com>
2015-08-22 22:08:17 +0530

    Done with A-instruction labeless

--- main.c
+++ main.c
@@ -2,6 +2,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+void modifyName(char *fname)
+{
+    int i=0;
+
+    while(fname[i]!='.')
+        i++;
+
+    fname[++i]='h';
+    fname[++i]='a';
+    fname[++i]='c';
+    fname[++i]='k';
+    fname[++i]='\0';
+}
+
 void dec_binary(int num,char bin[15])
 {
     int i,mod;
@@ -23,7 +37,35 @@ void dec_binary(int num,char bin[15])
     }
 }
 
-void scan1(FILE *fptr,FILE *inptr)
+void aInstruction(FILE *fptr,FILE *outPtr)
+{
+    int i=0,num;
+    char bin[15],dec[6],c;
+
+    do
+    {
+        fscanf(fptr,"%c",&c);
+
+        if(c=='\n')
+            dec[i]='\0';
+        else
+            dec[i]=c;
+        i++;
+
+    }while(c!='\n');
+
+    num=atoi(dec);
+    dec_binary(num,bin);
+
+    for(i=0;i<15;i++)
+        fprintf(outPtr,"%c",bin[i]);
+
+    fprintf(outPtr,"\n");
+
+}
+
+
+void scan1(FILE *fptr,FILE *outPtr)
 {
     int pc=0;
     char c;
@@ -33,7 +75,7 @@ void scan1(FILE *fptr,FILE *inptr)
         if(c=='\n'&&!newlineFlag)
             {
                 newlineFlag=true;
-                fprintf(inptr,"\n");
+                fprintf(outPtr,"\n");
                 pc++;
             }
 
@@ -46,11 +88,11 @@ void scan1(FILE *fptr,FILE *inptr)
                             while(c!='\n')
                                 fscanf(fptr,"%c",&c);
                             if(!newlineFlag)
-                            fprintf(inptr,"\n");
+                            fprintf(outPtr,"\n");
                             break;
 
             case ' ': break;
-            default: fprintf(inptr,"%c",c);
+            default: fprintf(outPtr,"%c",c);
                      newlineFlag=false;
                      break;
         }
@@ -59,21 +101,47 @@ void scan1(FILE *fptr,FILE *inptr)
 }
 
 
+void scan2(FILE *fptr,FILE *outPtr)
+{
+    char c;
+
+    while(fscanf(fptr,"%c",&c)!=EOF)
+        if(c=='@')
+        {
+            fprintf(outPtr,"0");
+            aInstruction(fptr,outPtr);
+        }
+}
+
 int main()
 {
-    FILE *fptr,*inptr;
+    FILE *fptr,*outPtr;
     char fname[20];
 
     printf("Enter the filename: ",fname);
     scanf("%s",fname);
 
     fptr=fopen(fname,"r");
-    inptr=fopen("main.asm","w");
+    outPtr=fopen("main.asm","w");
 
-    if(!fptr&&!inptr)
-    return 1;
+    if(!fptr&&!outPtr)
+        return 1;
 
     else
-    scan1(fptr,inptr);
+        scan1(fptr,outPtr);
+
+    fclose(fptr);
+    fclose(outPtr);
+
+    fptr=fopen("main.asm","r");
+    modifyName(fname);
+    outPtr=fopen(fname,"w");
+
+    if(!fptr&&!outPtr)
+        return 2;
+
+    else
+        scan2(fptr,outPtr);
+
     return 0;
 }
